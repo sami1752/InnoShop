17d6af28c0d507e87d77d3dbaa71bc24a30daaea
sami1752 <39447486+sami1752@users.noreply.github.com>
2021-03-14 22:39:58 -0500

    Creacion Api listar usuarios, modificar usuarios 2

---
+++ Back/Back/Migrations/20210315020254_Eliminacion C creacion Estado.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Back.Migrations
+{
+    public partial class EliminacionCcreacionEstado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "C",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "C",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
