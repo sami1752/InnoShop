ed77cfc861d3100fce1d320a0694d1baf9d0ef98
sami1752 <39447486+sami1752@users.noreply.github.com>
2021-04-12 01:29:38 -0500

    Avance apis SP

--- Back/Back/Controllers/SolicitudesController.cs
+++ Back/Back/Controllers/SolicitudesController.cs
@@ -1,5 +1,6 @@
 ﻿using Back.Models.Abstratos;
 using Back.Models.Entidades.Solicitudes;
+using Back.Models.Entidades.Solicitudes.Personalizadas;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -97,5 +98,186 @@ namespace Back.Controllers
                 return e.Message;
             }
         }
+
+
+
+        [HttpGet]
+        [Route("DetalleEstadosMontajes")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosMontajes>>> ListarDetalleEstadosMontajes() =>
+            await _context.ListarDetalleEstadosMontajes();
+
+        [HttpGet]
+        [Route("DetalleEstadosMontajes/{idDetalleEstadosMontajes}")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosMontajes>>> ListaDetalleEstadosMontajes(int DetalleEstadosMontajes) =>
+            await _context.ListaDetalleEstadosMontajes(DetalleEstadosMontajes);
+
+
+        [HttpPost]
+        [Route("DetalleEstadosMontajes")]
+        public async Task<Object> AgregarDetalleEstadosMontajes(DetalleEstadosMontajes DetalleEstadosMontajes)
+        {
+            try
+            {
+                DetalleEstadosMontajes = await _context.AgregarDetalleEstadosMontajes(DetalleEstadosMontajes);
+                return Ok(new { mensaje = DetalleEstadosMontajes });
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+        [HttpGet]
+        [Route("DetalleEstadosProductosPersoanlizados")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosProductosPersoanlizados>>>
+            ListarDetalleEstadosProductosPersoanlizados() =>
+            await _context.ListarDetalleEstadosProductosPersoanlizados();
+
+        [HttpGet]
+        [Route("DetalleEstadosProductosPersoanlizados/{DetalleEstadosProductosPersoanlizados}")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosProductosPersoanlizados>>>
+            ListaDetalleEstadosProductosPersoanlizados(int DetalleEstadosProductosPersoanlizados) =>
+            await _context.ListaDetalleEstadosProductosPersoanlizados(DetalleEstadosProductosPersoanlizados);
+
+        [HttpPost]
+        [Route("DetalleEstadosProductosPersoanlizados")]
+        public async Task<Object> AgregarDetalleEstadosProductosPersoanlizados
+            (DetalleEstadosProductosPersoanlizados DetalleEstadosProductosPersoanlizados)
+        {
+            try
+            {
+                DetalleEstadosProductosPersoanlizados =
+                    await _context.AgregarDetalleEstadosProductosPersoanlizados(DetalleEstadosProductosPersoanlizados);
+                return Ok(new { mensaje = DetalleEstadosProductosPersoanlizados });
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+
+        [HttpGet]
+        [Route("DetalleEstadosSolicitudPersonalizada")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosSolicitudPersonalizada>>>
+           ListarDetalleEstadosSolicitudPersonalizada() =>
+           await _context.ListarDetalleEstadosSolicitudPersonalizada();
+
+        [HttpGet]
+        [Route("DetalleEstadosSolicitudPersonalizada/{DetalleEstadosSolicitudPersonalizada}")]
+        public async Task<ActionResult<IEnumerable<DetalleEstadosSolicitudPersonalizada>>>
+            ListaDetalleEstadosSolicitudPersonalizada(int DetalleEstadosSolicitudPersonalizada) =>
+            await _context.ListaDetalleEstadosSolicitudPersonalizada(DetalleEstadosSolicitudPersonalizada);
+
+        [HttpPost]
+        [Route("DetalleEstadosSolicitudPersonalizada")]
+        public async Task<Object> AgregarDetalleEstadosSolicitudPersonalizada
+            (DetalleEstadosSolicitudPersonalizada DetalleEstadosSolicitudPersonalizada)
+        {
+            try
+            {
+                DetalleEstadosSolicitudPersonalizada =
+                    await _context.AgregarDetalleEstadosSolicitudPersonalizada(DetalleEstadosSolicitudPersonalizada);
+                return Ok(new { mensaje = DetalleEstadosSolicitudPersonalizada });
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+
+        [HttpDelete]
+        [Route("DetalleProductosSolicitud/{DetalleProductosSolicitud}")]
+        public async Task<Object> EliminarDetalleProductosSolicitud(int DetalleProductosSolicitud)
+        {
+            try
+            {
+                await _context.EliminarDetalleProductosSolicitud(DetalleProductosSolicitud);
+                return Ok(new { mensaje = "Eliminación Exitosa" });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("DetalleProductosSolicitud")]
+        public async Task<ActionResult<IEnumerable<DetalleProductosSolicitud>>>
+        ListarDetalleProductosSolicitud() =>
+            await _context.ListarDetalleProductosSolicitud();
+
+        [HttpGet]
+        [Route("DetalleProductosSolicitud/{DetalleProductosSolicitud}")]
+        public async Task<ActionResult<IEnumerable<DetalleProductosSolicitud>>>
+            ListaDetalleProductosSolicitud(int DetalleProductosSolicitud) =>
+            await _context.ListaDetalleProductosSolicitud(DetalleProductosSolicitud);
+
+        [HttpPost]
+        [Route("DetalleProductosSolicitud")]
+        public async Task<Object> AgregarDetalleProductosSolicitud
+            (DetalleProductosSolicitud DetalleProductosSolicitud)
+        {
+            try
+            {
+                DetalleProductosSolicitud =
+                    await _context.AgregarDetalleProductosSolicitud(DetalleProductosSolicitud);
+                return Ok(new { mensaje = DetalleProductosSolicitud });
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("DetalleProductosSolicitud/{DetalleProductosSolicitud}")]
+        public async Task<Object> EliminarDetallesMaterialesMontajes(int DetallesMaterialesMontajes)
+        {
+            try
+            {
+                await _context.EliminarDetallesMaterialesMontajes(DetallesMaterialesMontajes);
+                return Ok(new { mensaje = "Eliminación Exitosa" });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("DetallesMaterialesMontajes")]
+        public async Task<ActionResult<IEnumerable<DetallesMaterialesMontajes>>>
+        ListarDetallesMaterialesMontajes() =>
+            await _context.ListarDetallesMaterialesMontajes();
+
+        [HttpGet]
+        [Route("DetallesMaterialesMontajes/{DetallesMaterialesMontajes}")]
+        public async Task<ActionResult<IEnumerable<DetallesMaterialesMontajes>>>
+            ListaDetallesMaterialesMontajes(int DetallesMaterialesMontajes) =>
+            await _context.ListaDetallesMaterialesMontajes(DetallesMaterialesMontajes);
+
+        [HttpPost]
+        [Route("DetallesMaterialesMontajes")]
+        public async Task<Object> AgregarDetallesMaterialesMontajes
+            (DetallesMaterialesMontajes DetallesMaterialesMontajes)
+        {
+            try
+            {
+                DetallesMaterialesMontajes =
+                    await _context.AgregarDetallesMaterialesMontajes(DetallesMaterialesMontajes);
+                return Ok(new { mensaje = DetallesMaterialesMontajes });
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
     }
 }
--- Back/Back/Models/Abstratos/IServiciosSolicitudes.cs
+++ Back/Back/Models/Abstratos/IServiciosSolicitudes.cs
@@ -1,4 +1,5 @@
 ﻿using Back.Models.Entidades.Solicitudes;
+using Back.Models.Entidades.Solicitudes.Personalizadas;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,5 +18,91 @@ namespace Back.Models.Abstratos
         Task<DetalleCarritoDeCompras> BuscarDetalleCarritoDeComprasPorId(int id);
         Task<DetalleCarritoDeCompras> AgregarDetalleCarritoDeCompras(DetalleCarritoDeCompras detalleCarritoDeCompras);
         Task<DetalleCarritoDeCompras> EditarDetalleCarritoDeCompras(DetalleCarritoDeCompras detalleCarritoDeCompras);
+
+//--------
+        Task<DetalleEstadosMontajes> AgregarDetalleEstadosMontajes(DetalleEstadosMontajes DetalleEstadosMontajes);
+        Task<ActionResult<IEnumerable<DetalleEstadosMontajes>>> ListarDetalleEstadosMontajes();
+        Task<ActionResult<IEnumerable<DetalleEstadosMontajes>>> ListaDetalleEstadosMontajes(int id);
+
+        Task<DetalleEstadosProductosPersoanlizados> AgregarDetalleEstadosProductosPersoanlizados
+                    (DetalleEstadosProductosPersoanlizados DetalleEstadosProductosPersoanlizados);
+        Task<ActionResult<IEnumerable<DetalleEstadosProductosPersoanlizados>>>
+                    ListarDetalleEstadosProductosPersoanlizados();
+        Task<ActionResult<IEnumerable<DetalleEstadosProductosPersoanlizados>>>
+                    ListaDetalleEstadosProductosPersoanlizados(int id);
+
+        Task<DetalleEstadosSolicitudPersonalizada> AgregarDetalleEstadosSolicitudPersonalizada
+                    (DetalleEstadosSolicitudPersonalizada DetalleEstadosSolicitudPersonalizada);
+        Task<ActionResult<IEnumerable<DetalleEstadosSolicitudPersonalizada>>>
+                    ListarDetalleEstadosSolicitudPersonalizada();
+        Task<ActionResult<IEnumerable<DetalleEstadosSolicitudPersonalizada>>>
+                    ListaDetalleEstadosSolicitudPersonalizada(int id);
+
+        Task<DetalleProductosSolicitud> AgregarDetalleProductosSolicitud
+                    (DetalleProductosSolicitud DetalleProductosSolicitud);
+        Task<ActionResult<IEnumerable<DetalleProductosSolicitud>>> ListarDetalleProductosSolicitud();
+        Task<ActionResult<IEnumerable<DetalleProductosSolicitud>>> ListaDetalleProductosSolicitud(int id);
+        Task EliminarDetalleProductosSolicitud(int id);
+       
+
+
+        
+        Task<DetallesMaterialesMontajes> AgregarDetallesMaterialesMontajes
+                    (DetallesMaterialesMontajes DetallesMaterialesMontajes);
+        Task<ActionResult<IEnumerable<DetallesMaterialesMontajes>>> ListarDetallesMaterialesMontajes();
+        Task<ActionResult<IEnumerable<DetallesMaterialesMontajes>>> ListaDetallesMaterialesMontajes(int id);
+        Task EliminarDetallesMaterialesMontajes(int id);
+        
+        
+        
+
+
+        Task<DetallesMaterialesSolicitudesPersonalizadas> AgregarDetallesMaterialesSolicitudesPersonalizadas
+                    (DetallesMaterialesSolicitudesPersonalizadas DetallesMaterialesSolicitudesPersonalizadas);
+        Task<ActionResult<IEnumerable<DetallesMaterialesSolicitudesPersonalizadas>>>
+                    ListarDetallesMaterialesSolicitudesPersonalizadas();
+        Task<ActionResult<IEnumerable<DetallesMaterialesSolicitudesPersonalizadas>>>
+                    ListaDetallesMaterialesSolicitudesPersonalizadas(int id);
+        Task EliminarDetallesMaterialesSolicitudesPersonalizadas(int id);
+        
+        
+        
+        
+        Task<DetallesProductosMontajes> AgregarDetallesProductosMontajes
+                    (DetallesProductosMontajes DetallesProductosMontajes);
+        Task<ActionResult<IEnumerable<DetallesProductosMontajes>>> ListarDetallesProductosMontajes();
+        Task<ActionResult<IEnumerable<DetallesProductosMontajes>>> ListaDetallesProductosMontajes(int id);
+        Task EliminarDetallesProductosMontajes(int id);
+        
+        
+
+
+        Task<ActionResult<IEnumerable<Montajes>>> ListarMontajes();
+        Task<Montajes> BuscarMontajes(int id);
+        Task<Montajes> AgregarMontajes(Montajes Montajes);
+        Task<Montajes> EditarMontajes(Montajes Montajes);
+        
+        
+        
+        Task<PrecioMontajes> AgregarPrecioMontajes(PrecioMontajes PrecioMontajes);
+        Task<ActionResult<IEnumerable<PrecioMontajes>>> ListarPrecioMontajes();
+        Task<ActionResult<IEnumerable<PrecioMontajes>>> ListaPrecioMontajes(int id);
+        
+        
+        
+
+        Task<RespuestasSolicitudesPersonalizadas> AgregarRespuestasSolicitudesPersonalizadas
+                    (RespuestasSolicitudesPersonalizadas RespuestasSolicitudesPersonalizadas);
+        Task<ActionResult<IEnumerable<RespuestasSolicitudesPersonalizadas>>>
+                    ListarRespuestasSolicitudesPersonalizadas();
+       
+        
+        
+        Task<ActionResult<IEnumerable<SolicitudPersonalizada>>> ListarSolicitudPersonalizada();
+        Task<SolicitudPersonalizada> BuscarSolicitudPersonalizada(int id);    
+        Task<SolicitudPersonalizada> AgregarSolicitudPersonalizada(SolicitudPersonalizada SolicitudPersonalizada);
+        Task<SolicitudPersonalizada> EditarSolicitudPersonalizada(SolicitudPersonalizada SolicitudPersonalizada);
+
+
     }
 }
