383ac51afb3ad5a93af0a94dcdab7ce33ca1715f
sami1752 <39447486+sami1752@users.noreply.github.com>
2021-04-26 15:36:52 -0500

    Avances SP Adnmi

---
+++ Front/src/app/components/admin/solicitudes/SolicitudesPersonalizadas/detalle-solicitud-personalizada/listar-detalle-producto-solicitud/listar-detalle-producto-solicitud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-detalle-producto-solicitud',
+  templateUrl: './listar-detalle-producto-solicitud.component.html',
+  styleUrls: ['./listar-detalle-producto-solicitud.component.css']
+})
+export class ListarDetalleProductoSolicitudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
