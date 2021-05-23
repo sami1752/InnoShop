b85556506c61f06b39d564cf3807752ce2615bfd
cejis18 <jdtoro949@misena.edu.co>
2021-05-22 23:59:33 -0400

    mAS DE VENTAS

--- Front/src/app/components/usuarios/clientes/solicitudes/carrito-compras/finalizar-compra/finalizar-compra.component.ts
+++ Front/src/app/components/usuarios/clientes/solicitudes/carrito-compras/finalizar-compra/finalizar-compra.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ICreateOrderRequest, IPayPalConfig } from 'ngx-paypal';
 import { CarritoDeCompras } from 'src/app/models/carrito-de-compras';
 import { DetalleCarritoDeCompras } from 'src/app/models/detalle-carrito-de-compras';
+import { DetalleVentasProducto } from 'src/app/models/Ventas/detalle-ventas-producto';
+import { Venta } from 'src/app/models/Ventas/venta';
 import { CarritoDeComprasService } from 'src/app/services/carrito-de-compras.service';
 import { ProductoService } from 'src/app/services/producto.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { VentasService } from 'src/app/services/ventas.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -14,21 +17,25 @@ import { environment } from 'src/environments/environment';
 })
 export class FinalizarCompraComponent implements OnInit {
 
-  constructor(public carritoDeComprasService:CarritoDeComprasService,public productosService:ProductoService, public usuarioService:UsuarioService) { }
+  constructor(public carritoDeComprasService:CarritoDeComprasService,
+    public productosService:ProductoService, 
+    public usuarioService:UsuarioService,
+    public ventasService:VentasService) { }
 
   public payPalConfig ?: IPayPalConfig;
   public PaypalButtons: boolean;
+  public detalleVentaProducto:DetalleVentasProducto
 
   ngOnInit(): void {
     this.usuarioService.obtenerPerfil().subscribe(
       (res:any)=>{
-        this.initConfig()
+        this.initConfig(res.Id)
         this.carritoDeComprasService.CarritoDeComprasUsuario(res.Id)
         this.carritoDeComprasService.listarDetalleCarrito(res.id)
       },err=>{}
     )
   }
-    private initConfig(): void {
+    private initConfig(idUsuario:string): void {
       this.payPalConfig = {
           currency: 'EUR',
           clientId: environment.clienteId,
@@ -58,6 +65,18 @@ export class FinalizarCompraComponent implements OnInit {
           onApprove: (data, actions) => {
               console.log('onApprove - transaction was approved, but not authorized', data, actions);
               actions.order.get().then(details => {
+                 /* this.ventasService.AgregarVenta().subscribe(
+                    (res:Venta)=>{
+                      this.carritoDeComprasService.listarDetalleCarrito(idUsuario)
+                      this.carritoDeComprasService.listaDetalleCarritoCompras.forEach((dCar:DetalleCarritoDeCompras)=>{
+                      this.detalleVentaProducto.Cantidad = dCar.Cantidad
+                      this.detalleVentaProducto.IdProducto = dCar.IdProducto
+                      this.detalleVentaProducto.IdVenta = res.IdVenta
+                      this.detalleVentaProducto.SubTotal = 0
+                      })
+                      //this.ventasService.agregarDetalleVenta()
+                    },err=>{alert("Error")}
+                  )*/
                   console.log('onApprove - you can get full order details inside onApprove: ', details);
               });
 
@@ -67,7 +86,6 @@ export class FinalizarCompraComponent implements OnInit {
           },
           onCancel: (data, actions) => {
               console.log('OnCancel', data, actions);
-
           },
           onError: err => {
               console.log('OnError', err);
